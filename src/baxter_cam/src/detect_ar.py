1afd3f653d7605622eb66001a93beb07c4036722
Vidish Gupta <vidishgupta@berkeley.edu>
2021-12-09 13:47:26 -0800

    added ar tracking code to baxter_cam

--- src/baxter_cam/src/detect_ar.py
+++ src/baxter_cam/src/detect_ar.py
@@ -9,63 +9,45 @@
 import rospy
 import tf2_ros
 import sys
+import roslaunch
 
 from geometry_msgs.msg import Twist, Vector3, Quaternion
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 
 #Define the method which contains the main functionality of the node.
-def controller(ar_frame, hand_frame):
-  """
-  Controls a turtlebot whose position is denoted by turtlebot_frame,
-  to go to a position denoted by target_frame
-  Inputs:
-  - turtlebot_frame: the tf frame of the AR tag on your turtlebot
-  - target_frame: the tf frame of the target AR tag
-  """
+def transform_generator(ar_frame, gripper_frame, time):
+  
+  ## INIT
 
-  ################################### YOUR CODE HERE ##############
+  node = roslaunch.core.Node('ar_track_alvar', 'ar_track_alvar')
+  launch = roslaunch.scriptapi.ROSLaunch()
+  launch.start()
+
+  process = launch.launch(node)
+  print(process.is_alive())
+
+  ### FINISH INIT
 
   #Create a publisher and a tf buffer, which is primed with a tf listener
-  #pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
   tfBuffer = tf2_ros.Buffer()
   tfListener = tf2_ros.TransformListener(tfBuffer)
   
-  # Create a timer object that will sleep long enough to result in
-  # a 10Hz publishing rate
-  r = rospy.Rate(10) # 10hz
-
-  K1 = 0.3
-  K2 = 1
-  # Loop until the node is killed with Ctrl-C
+  # Loop until we have a transform
   while not rospy.is_shutdown():
     try:
-      trans = tfBuffer.lookup_transform(ar_frame, hand_frame, rospy.Time())
+      trans = tfBuffer.lookup_transform(ar_frame, gripper_frame, time)
 
-      # Process trans to get your state error
-      # Generate a control command to send to the robot
-      # rospy.loginfo(trans)
+      print("translation: " + str(trans.transform.translation))
+      print("translation: " + str(trans.transform.rotation))
 
-      control_command = Twist(Vector3(trans.transform.translation.x * K1, trans.transform.translation.y * K1, trans.transform.translation.z * K1), Quaternion(trans.transform.rotation.x * K2, trans.transform.rotation.y * K2, trans.transform.rotation.z * K2, trans.transform.rotation.w * K2))# Generate this
-
-      #################################### end your code ###############
-
-      pub.publish(control_command)
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
       pass
-    # Use our rate object to sleep until it is time to publish again
-    r.sleep()
 
-      
 # This is Python's sytax for a main() method, which is run by default
 # when exectued in the shell
 if __name__ == '__main__':
-  # Check if the node has received a signal to shut down
-  # If not, run the talker method
-
-  #Run this program as a new node in the ROS computation graph 
-  #called /turtlebot_controller.
-  rospy.init_node('turtlebot_controller', anonymous=True)
 
   try:
-    controller(sys.argv[1], sys.argv[2])
+    controller(sys.argv[1], sys.argv[2], rospy.Time())
   except rospy.ROSInterruptException:
     pass
